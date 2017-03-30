8d352b7fc711fd433b4a6cd8c077ad5b6b041251
Rob Shimits | reshimits <r.e@shimits.com>
2017-03-30 10:53:53 -0400

    ACME CSS/HTML

---
+++ index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+	<!-- Basic Page Needs
+    ================================================== -->
+	<meta charset="utf-8">
+	<title>< ACME > Real Lighting Performance at the Right Price</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="SHIMITS Design/Development">
+
+	<!-- Mobile Specific Metas
+    ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="format-detection" content="telephone=no" />
+
+	<!-- CSS
+    ================================================== -->
+    <link href="css/bootstrap-theme-full.css" rel="stylesheet">
+    <!-- <link href="../mag_trs_design/skin/frontend/acme/default/css/acme-theme.css" rel="stylesheet"> -->
+    <link href="css/acme-theme.css" rel="stylesheet">
+
+	<!-- Javascript [primary]
+    ================================================== -->
+  	<script src="js/modernizr.custom.63321.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+	<!-- Favicons
+    ================================================== -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="icon" sizes="32x32" href="img/favicon-32.png">
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon-152.png">
+
+</head>
+
+<body class="home">
+
+    <!-- Header
+    ================================================== -->
+
+    <div class="head">
+        <a href="#"><img src="img/logo-a-white.svg" alt="ACME A" border="0"/></a>
+    </div>
+
+
+    <!-- Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+
+
+
+        <div class="section intro">
+            <div class="container">
+                <div class="row">
+                    <div class="feature col-xs-12">
+                        <img src="img/logo-glow2.svg" class="img-responsive" alt="ACME" border="0"/>
+                    </div>
+                </div>
+            </div>
+            <div class="topographicMap"></div>
+        </div>
+        
+
+        <div class="section bg-red">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 centered">
+                        <h1>Real Lighting Performance</h1>
+                        <div class="script-rightprice"><img src="img/script-rightprice.svg" class="img-responsive" alt="at the Right Price" border="0"/></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="section bg-cardboard bg-cardboard1 ledbulbs">
+            <a name="ledbulbs"></a>
+            <div class="container">
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-sm-offset-3"><hr class="aligncenter"></div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 centered">
+                        <h2>LED Bulbs</h2>
+                        <h3>Every Fitment. Every Color Combination.</h3>
+                    </div>  
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-md-7 pull-right">
+                        <img src="img/category-ledbulbs.png" class="img-responsive" alt="LED Bulbs" border="0"/>
+                        <h4>Black Series Switchbacks ... Red Rocket Reverse lights, and everything in between.</h4>
+                        <p>Unlimited variety. Different fitments from front to back. Different circuit types. Different optical styles. There are many ways to build an LED based bulb to replace an incandescent, and regardless of size, ACME has it covered. From the most popular T10 and 3157 fitments to 921 and Festoons, we offer something that's fashionable and shines bright. Most of our products use domestic or Korean LED chips, but on some specialty models we do integrate CREE and Lumiled LEDs. White. Amber. Red. Switchback, we've got it all! </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-5">
+                        <div class="well">
+                            <img src="img/chart-ledbulbs2x.png" class="img-responsive" alt="LED Bulbs Statistics" border="0"/>
+                        </div>  
+                        <hr class="alt aligncenter"> 
+                        <a href="" type="button" class="btn btn-block btn-primary">< Shop LED Bulbs ></a> 
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4"><hr class="aligncenter"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section bg-cardboard bg-cardboard2 ledaccent">
+            <a name="ledaccent"></a>
+            <div class="container">
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-sm-offset-3"><hr class="aligncenter"></div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 centered">
+                        <h2>LED Accent Lighting</h2>
+                        <h3>SuperFlex, Sequential, and Switchback Series.</h3>
+                    </div>  
+                </div>
+                <div class="section">
+                    <div class="col-sm-6 col-md-7">
+                        <img src="img/category-ledaccent.png" class="img-responsive" alt="LED Accent Lighting" border="0"/>
+                        <h4>Single Color, RGB, Switchback, and Sequential Tech. That ought to cover it!</h4>
+                        <p>The desire to re-create that expensive OEM LED look on a budget. Our LED Accent Lighting lineup definitely delivers, and from the time you turn 'em on we're sure you'll be sold. No fancy phosphor coatings here, just tightly packed LEDs on a flexible (and paint-able) circuit board that packs a sure punch when illuminated. No matter the style, our setups are designed with external drivers for easy serviceability outside the headlamp enclosure, and can be ordered in most any strip length. No frills here. We use only proven, mature technology and production techniques to produce a good looking and reliable product for you. </p>
+                    </div>
+                    <div class="col-sm-6 col-md-5">
+                        <div class="well">
+                            <img src="img/chart-ledaccent2x.png" class="img-responsive" alt="LED Accent Lighting Statistics" border="0"/>
+                        </div>  
+                        <hr class="alt aligncenter"> 
+                        <a href="" type="button" class="btn btn-block btn-primary">< Shop LED Accent Lighting ></a>   
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4"><hr class="aligncenter"></div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="section bg-cardboard bg-cardboard1 hidbulbs">
+            <a name="hidbulbs"></a>
+            <div class="container">
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-sm-offset-3"><hr class="aligncenter"></div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 centered">
+                        <h2>HID Bulbs</h2>
+                        <h3>Above average output at a below average cost.</h3>
+                    </div>  
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-md-7 pull-right">
+                        <img src="img/category-hidbulbs.png" class="img-responsive" alt="HID Bulbs" border="0"/>
+                        <h4>H1, H3, H4, H7, H8, H9, H11A, H11B, H13, 9004, 9005, 9006, 9012. All Kelvin Ratings from 3000K to 8000K. </h4>
+                        <p>While LED headlight bulbs continue to become more and more popular, the fact is that the more mature HID technology still proves better when it comes to putting light on the road. ACME's Standard Series HID headlight bulbs are every bit as good as the best in the aftermarket due to the simple fact the technology reached its peak many moons ago. Therefore, we (and our customers) don't need to be the industry's top spenders to access current-day production techniques and raw materials. Here, ACME believes that to strive for average is synonymous with not trying at all. Our Standard bulbs deliver above average performance at a below average price.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-5">
+                        <div class="well">
+                            <img src="img/chart-hidbulbs2x.png" class="img-responsive" alt="HID Bulbs Statistics" border="0"/>
+                        </div>    
+                        <hr class="alt aligncenter"> 
+                        <a href="" type="button" class="btn btn-block btn-primary">< Shop HID Bulbs ></a> 
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4"><hr class="aligncenter"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section bg-cardboard bg-cardboard2 hidballasts">
+            <a name="hidballasts"></a>
+            <div class="container">
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-sm-offset-3"><hr class="aligncenter"></div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 centered">
+                        <h2>HID Ballasts</h2>
+                        <h3>Svelte and Speedy or Big and Bright as the Sun.</h3>
+                    </div>  
+                </div>
+                <div class="section">
+                    <div class="col-sm-6 col-md-7">
+                        <img src="img/category-hidballasts.png" class="img-responsive" alt="HID Ballasts" border="0"/>
+                        <h4>Our line of HID Headlight Ballasts includes 35W, 55W, and 75W power plants.</h4>
+                        <p>We may be the bargain brand, but we don't fill our ballasts with sand. While there's something to be said for getting a good deal, ACME insists that it's only OK to cut costs when it can't affect your safety. Becoming the industry standard for value means delivering the right proportion of bang for the buck; so while our ballasts aren't anywhere near the cheapest, they deliver stable quality that you can confide in. The SpeedyStart 35w competes with more expensive options from competitors like Hylux, Morimoto, and CNLight. On the other hand, nobody else offers anything comparable to the 75W Dynamite's designed for off-road use. </p>
+                    </div>
+                    <div class="col-sm-6 col-md-5">
+                        <div class="well">
+                            <img src="img/chart-hidballasts2x.png" class="img-responsive" alt="HID Ballasts Statistics" border="0"/>
+                        </div>    
+                        <hr class="alt aligncenter"> 
+                        <a href="" type="button" class="btn btn-block btn-primary">< Shop HID Ballasts ></a> 
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4"><hr class="aligncenter"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section bg-cardboard bg-cardboard1 hidprojectors">
+            <a name="hidprojectors"></a>
+            <div class="container">
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-sm-offset-3"><hr class="aligncenter"></div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 centered">
+                        <h2>HID Projectors</h2>
+                        <h3>Light on the road. Where it belongs.</h3>
+                    </div>  
+                </div>
+                <div class="section">
+                    <div class="col-xs-12 col-sm-6 col-md-7 pull-right">
+                        <img src="img/category-hidprojectors.png" class="img-responsive" alt="HID Projectors" border="0"/>
+                        <h4>Our projectors make it possible to cut glare and cut cost at the same time. </h4>
+                        <p>The lineup includes common fitments for all bulb sizes including D2S, D1S, D3S, and H1. Designed with three things in mind. First and foremost, light output - because if you're going to upgrade your headlamps and up the output, it's important that the added light end up on the ground where it belongs, not in the eyes of oncoming traffic. Second, ease of installation. Because if its not easy to install, users will opt-out or face errors that could compromise the result. Lastly, value. Because we're not Morimoto, nor are we trying to be the benchmark. But by acquiring their old tooling and putting it back into service - we're able to deliver yesterday's benchmark projectors to you, today, at a fraction of what they sold for when they were the latest and greatest. </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-5">
+                        <div class="well">
+                            <img src="img/chart-hidprojectors2x.png" class="img-responsive" alt="HID Projectors Statistics" border="0"/>
+                        </div>    
+                        <hr class="alt aligncenter"> 
+                        <a href="" type="button" class="btn btn-block btn-primary">< Shop HID Projectors ></a> 
+                    </div>
+                </div>
+                <div class="section">
+                    <div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4"><hr class="aligncenter"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section bg-red">
+            <div class="section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 centered">
+                            <h1>The Acme Advantage</h1>
+                            <h4 class="text-tanlight large">Long story short</h4>
+                            <p class="large">Performance lighting for the automotive world is popular. With millions of cars and trucks on the road rocking headlight tech designed during the stone ages; inevitably the demand for something that will allow drivers to see better while traveling at night is there. Likewise, we recognize the fact that many if not most of the vehicles on the road equipped with inadequate headlights aren't brand new cars. They are owned, driven, and maintained by everyday people. Everyday people who just want something better and can afford it on an average joe budget. </p>
+
+                            <p class="large">ACME Delivers real lighting performance at the right price. By using technology and left-over tooling taken from yesterday's high-end; we make today's average, we make it affordable, and we're proud of it! And unless you're a lighting snob with a lumen meter built into your brain, we're pretty sure you won't see the subtle difference between a product you bought from ACME at a bargain and the premium brands for twice the price. Our production lines in southern China turn out some of the most recognizable parts in the industry today. Search Amazon, Alibaba, or almost any online retail store who sells HID kits for cars or LED kits for trucks and you'll probably find something from our line private labeled under brand X, Y, or Z. Look closer though and you'll see that it's all the same stuff, and it all comes out of the same place. But that's A-okay, because even though it may be basic, it works well and it's affordable, and that's why so many brands trust us to deliver the goods. HID and LED Headlight Projectors, HID Ballasts, HID Bulbs, LED Bulbs, and LED Accent Lighting. We supply generic, private-label, and self-branded parts to dealers all over the world. We do it all and our mission always was and always will be to deliver Real Performance at the Right Price. </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+
+    <!-- End Primary Page Layout
+    ==================================================
+    ==================================================
+    ================================================== -->
+
+    <?php require('shared/footer.html');?>
+
+
+    
+
+	<!-- Javascript [secondary]
+  	================================================== -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>    
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+
+    <!-- ALL: iOS Viewport Fix -->
+    <!-- <script type="text/javascript">
+    var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+    if(iOS){
+
+       function iosVhHeightBug() {
+          var height = $(window).height();
+          $(".intro").css('height', height);
+          $(".intro .feature").css('margin-top', height);
+       }
+
+       iosVhHeightBug();
+       $(window).bind('resize', iosVhHeightBug);
+    }  
+    </script>  -->
+    
+    
+
+
+    <!-- End Document
+    ================================================== -->
+
+</body>
+</html>
